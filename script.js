377e75a46f25ce63c4b407f4715bdfa7ab708c53
Abiner Maleiane Jr <abinermaleianejr@gmail.com>
2024-02-04 07:31:01 -0800

    I

--- script.js
+++ script.js
@@ -1,44 +1,104 @@
-const key = "849ff98095c0d7d62e387e1b49b4941f"
-let cidade
-var data = ""
+const apiKey = "";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
-function buttonClick() {
- cidade = document.querySelector(".cidade").value
-    console.log(cidade)
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
 
-    findCity(cidade)
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
-}
+const weatherContainer = document.querySelector("#weather-data");
 
-async function findCity() {
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
 
-    try {
-       const resposta = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + key + "&lang=pt&units=metric" )
-       
-       if (!resposta.ok) {
-          throw new Error("Erro ao obter dados meteorológicos");
-       }
- 
-        data = await resposta.json();
-       showWeather(data);
-    } catch (erro) {
-       console.log("Erro na requisição:"+ erro.message);
-    }
- }
- 
-function showWeather() {
-    console.log(data)      
-    document.querySelector(".cidade").innerHTML = `Tempo em ${data.main.name}`
-    document.querySelector(".temperatura").innerHTML = `${Math.floor(data.main.temp)}`
-    document.querySelector(".humidade").innerHTML = `${(data.main.humidity)}` 
-    
-    document.querySelector(".ceu").innerHTML = `${(data.weather[0].description)}`
-    document.querySelector(".vento").innerHTML = `${(data.wind.speed)}`
-    document.querySelector(".imagemceu").scr = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
-    document.querySelector(".imagemvento").scr = "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-wind-icon-png-image_4184509.jpg"
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
 
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
 
-    
+const getWeatherData = async (city) => {
+  toggleLoader();
 
-}
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  toggleLoader();
+
+  return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  // Change bg image
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+  weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+
+    showWeatherData(city);
+  });
+});
