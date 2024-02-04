41be2cffe26dd754ac93b62c61558f7f7f07016a
Abiner Maleiane Jr <abinermaleianejr@gmail.com>
2024-02-04 07:43:39 -0800

    v5

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ async function findCity() {
 function showWeather() {
     console.log(data)      
     document.querySelector(".cidade").innerHTML = `Tempo em ${data.main.name}`
-    document.querySelector(".temperatura").innerHTML = `${Math.floor(data.main.temp)}`
-    document.querySelector(".humidade").innerHTML = `${(data.main.humidity)}` 
+    document.querySelector(".temperatura").innerHTML = `${Math.floor(data.main.temp)} ºC`
+    document.querySelector(".humidade").innerHTML = `${(data.main.humidity)} %` 
     
     document.querySelector(".ceu").innerHTML = `${(data.weather[0].description)}`
-    document.querySelector(".vento").innerHTML = `${(data.wind.speed)}`
+    document.querySelector(".vento").innerHTML = `${(data.wind.speed)} Km/h`
     document.querySelector(".imagemceu").scr = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
     document.querySelector(".imagemvento").scr = "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-wind-icon-png-image_4184509.jpg"
 
